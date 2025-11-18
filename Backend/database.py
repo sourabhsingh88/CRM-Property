26e1ea0ce4493e27e5d540f6038dc8e10680e7d6
sourabhsingh88 <sourabhsinghmandloi122@gmail.com>
2025-11-18 14:11:46 +0530

    Remove secrets from git history

--- Backend/database.py
+++ Backend/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:root@localhost:3306/flipr_db")
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 engine = create_engine(
     DATABASE_URL,
